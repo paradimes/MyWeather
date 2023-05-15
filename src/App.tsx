e53ec27edb55b436da36e9d96d52ecefe4f479ac
AM <test@test.com>
2023-05-15 19:11:20 -0400

    working but still a work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ListItem from "./components/ListItem";
+import Axios, { AxiosResponse } from "axios";
+
+const DEVELOPER_KEY = "YOUR_DEVELOPER_KEY";
+
+interface WeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
 
 export default function App() {
-  const [data, setData] = useState("");
-  const [location, setLocation] = useState("Toronto");
-
-  const DEVELOPER_KEY = "YOUR_DEVELOPER_KEY";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${DEVELOPER_KEY}&units=metric`;
+  const [location, setLocation] = useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorResponse, setErrorResponse] = useState("");
+  const [dataArray, setDataArray] = useState([]);
 
   function handleChange(event: {
     target: { value: React.SetStateAction<string> };
   }) {
     setLocation(event.target.value);
   }
-  console.log("location", location);
 
-  // useEffect(() => {
-  //   fetch(url)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       // setData(data.map())
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // }, [url]);
-
-  // console.log("data", data);
-
-  // :root {
-  //   --bg_main: #0a1f44
-  //   --text_light: #fff
-  //   --text_med: #53627c
-  //   --text_dark: #1e2432
-  //   --red: #ff1e42
-  //   --darkred: #c3112d
-  //   --orange: #ff8c00
-  // }
+  async function getWeather() {
+    setIsLoading(true);
+    setErrorResponse("");
+    await Axios({
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${DEVELOPER_KEY}&units=metric`,
+      method: "GET",
+    })
+      .then((response) => {
+        setIsLoading(false);
+        setData(response.data);
+        setDataArray([...dataArray, response.data]);
+      })
+      .catch(function (error) {
+        setIsLoading(false);
+        setErrorResponse(error.response.data);
+      });
+  }
+  console.log("data = ", data);
+  console.log("errorResponse = ", errorResponse);
+  console.log("dataArray", dataArray);
 
   return (
     <main className="border-4 border-cyan-500 flex flex-col h-screen overflow-auto bg-[#222] text-white items-center pt-10">
@@ -63,54 +112,86 @@ export default function App() {
               onChange={handleChange}
               // autoFocus
             />
-            <button
-              type="submit"
-              className=" bg-slate-700 text-base h-full tracking-widest px-4 rounded-md hover:bg-slate-500 max-[700px]:w-full max-[700px]:mt-5 max-[700px]:mb-0 max-[700px]:mx-0 "
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              Search
-            </button>
+            {!location ? (
+              <button
+                type="submit"
+                className=" bg-slate-700 text-base h-full tracking-widest px-4 rounded-md max-[700px]:w-full max-[700px]:mt-5 max-[700px]:mb-0 max-[700px]:mx-0 cursor-not-allowed opacity-50"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                Search
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className=" bg-slate-700 text-base h-full tracking-widest px-4 rounded-md hover:bg-slate-500 max-[700px]:w-full max-[700px]:mt-5 max-[700px]:mb-0 max-[700px]:mx-0 "
+                onClick={(e) => {
+                  e.preventDefault();
+                  getWeather();
+                }}
+              >
+                Search
+              </button>
+            )}
           </form>
-          <div
-            id="msg"
-            className=" max-w-md min-h-[40px] text-sm font-bold text-red-700 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-3 max-[500px]:text-xs max-[700px]:static max-[700px]:min-h-0"
-          >
-            ...The fox jumped over the lorem ipsum dolor sit amet The fox jumped
-            over the lorem ipsum dolor sit amet
+          {errorResponse ? (
+            <div
+              id="msg"
+              className=" max-w-md min-h-[40px] text-sm font-bold text-red-700 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-3 max-[500px]:text-xs max-[700px]:static max-[700px]:min-h-0"
+            >
+              Error {errorResponse.cod}: {errorResponse.message}. Please enter a
+              valid city name.
+            </div>
+          ) : null}
+        </div>
+      </section>
+
+      {dataArray ? (
+        <section
+          id="api-section"
+          className="mt-16 mb-5 mx-0 max-[700px]:mt-5 border-4 border-green-600"
+        >
+          <div id="container" className="">
+            <ul
+              id="cities"
+              className="grid gap-y-8 gap-x-5 grid-cols-4 max-[1000px]:grid-cols-3 max-[700px]:grid-cols-2 max-[500px]:grid-cols-1 "
+            >
+              {dataArray.map((cityData) => {
+                return <WeatherCard isLoading={isLoading} data={cityData} />;
+              })}
+            </ul>
           </div>
-        </div>
-      </section>
-      <section
-        id="api-section"
-        className="mt-16 mb-5 mx-0 max-[700px]:mt-5 border-4 border-green-600"
-      >
-        <div id="container" className="">
-          <ul
-            id="cities"
-            className="grid gap-y-8 gap-x-5 grid-cols-4 max-[1000px]:grid-cols-3 max-[700px]:grid-cols-2 max-[500px]:grid-cols-1 "
-          >
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            {/* <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem /> */}
-          </ul>
-        </div>
-      </section>
+        </section>
+      ) : null}
     </main>
   );
 }
+
+const WeatherCard = ({
+  data,
+  isLoading,
+}: {
+  data: WeatherResponse;
+  isLoading: boolean;
+}) => {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!data) {
+    return null;
+  }
+
+  const { main, name, sys, weather } = data;
+  const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+  return (
+    <ListItem
+      locationCity={name}
+      locationCountry={sys.country}
+      temperature={Math.round(main.temp)}
+      iconSrc={icon}
+      iconAlt={weather[0]["description"]}
+      iconCaption={weather[0]["description"]}
+    />
+  );
+};
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,44 +1,50 @@
-import React from "react";
+// import React from "react";
 
-export default function ListItem() {
+type ListItemProps = {
+  locationCity: string;
+  locationCountry: string;
+  temperature: number;
+  iconSrc: string;
+  iconAlt?: string;
+  iconCaption: string;
+};
+
+export default function ListItem({
+  locationCity,
+  locationCountry,
+  temperature,
+  iconSrc,
+  iconAlt,
+  iconCaption,
+}: ListItemProps) {
   return (
     <li>
-      <div className="border-4 border-red-600  flex flex-col h-fit py-5 px-[10%] rounded-3xl bg-white text-slate-600">
-        <div id="city" className="pb-3 text-[20px] ">
-          Athens
+      <div className="border-4 border-red-600 min-w-[200px] flex flex-col h-full py-5 px-[10%] rounded-3xl bg-white text-slate-600">
+        <div id="city" className="pb-3 text-[20px]">
+          {locationCity}
           <sup className="ml-1 text-xs px-[0.6em] py-[0.2em] rounded-[30px] bg-orange-500 text-white">
-            GR
+            {locationCountry}
           </sup>
         </div>
         <div
           id="temp"
           className="text-7xl font-[bold] text-slate-800 max-[700px]:text-5xl  "
         >
-          11
+          {temperature}
           <sup className="text-[0.5em]">°C </sup>
         </div>
         <figure id="icon" className="">
           <img
             id="city-icon"
-            src=""
-            alt=""
+            src={iconSrc}
+            alt={iconAlt}
             className="w-[100px] h-[100px] mt-2.5"
           />
           <figcaption className="uppercase tracking-wider mt-2.5  ">
-            Very heavy rain
+            {iconCaption}
           </figcaption>
         </figure>
       </div>
     </li>
   );
 }
-
-// :root {
-//   --bg_main: #0a1f44
-//   --text_light: #fff ---> text-white
-//   --text_med: #53627c ---> text-slate-600
-//   --text_dark: #1e2432 --- text-slate-800
-//   --red: #ff1e42 --- text-red-500
-//   --darkred: #c3112d --- text-red-700
-//   --orange: #ff8c00 --- text-orange-500
-// }
